0b21902002baf4c71a1877f97a8eba3eb464ee31
Shobhit Sharma <shobhitss172@gmail.com>
2026-04-16 10:33:25 +0530

    admin created

---
+++ backend/modal/Admin.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: {type: String, required: true, enum: ['admin', 'super_admin'], default: 'admin'},
+    isActive: {type: Boolean, default: true},
+    lastlogin: {type: Date},
+    permissions: {
+        userMangament: {type: Boolean, default: true},
+        doctorManagement: {type: Boolean, default: true},
+        paymentManagement: {type: Boolean, default: true},
+        analytics: {type: Boolean, default: true},
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Admin', adminSchema);
